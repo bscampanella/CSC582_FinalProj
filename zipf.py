128a0062721b0401589d524ba5f895b62eaf1f55
AlexanderRodgers <alexedrodgers@gmail.com>
2021-05-31 16:48:56 -0700

    improved zipf calculations

--- zipf.py
+++ zipf.py
@@ -5,46 +5,90 @@ from nltk.corpus import stopwords
 from nltk.tokenize import MWETokenizer
 stop_words = set(stopwords.words('english'))
 import spacy
-nlp = spacy.load('en_core_web_sm')
 import re
+import math
 
-# def create_regex(word):
-#     return ('/([%s])/g' % word)
+KEEP_ENTS = ['DATE', 'PERSON', 'GPE', 'ORG', 'NORP', 'WORK_OF_ART', 'PRODUCT']
+
+class EntityRetokenizeComponent:
+    def __init__(self, nlp):
+        pass
+
+    def __call__(self, doc):
+        with doc.retokenize() as retokenizer:
+            for ent in doc.ents:
+                if ent in KEEP_ENTS:
+                    retokenizer.merge(doc[ent.start:ent.end])
+        return doc
+
+nlp = spacy.load('en_core_web_sm')
+retokenizer = EntityRetokenizeComponent(nlp)
+nlp.tokenizer.rules = {key: value for key, value in nlp.tokenizer.rules.items() if "'" not in key and "’" not in key and "‘" not in key}
+assert [t.text for t in nlp("can't")] == ["can't"]
 
 def read_file(filepath):
     f = open(filepath, 'r')
     return f.read()
-
-def most_common_words(raw_text, entities):
-
-    token_pairs = [tuple(e.split()) for e in entities]
-
-    tokenizer = MWETokenizer(token_pairs, separator=' ')
-
-    tokenized_words = word_tokenize(raw_text)
-
-    cleaned_words = []
-
-    for w in tokenized_words:
-        w = w.lower()
-        if w not in stop_words and w not in """,.'"!?;:`~--()""":
-            cleaned_words.append(w)
     
-    tokenized_words = tokenizer.tokenize(raw_text.split())
+word_freq = {}
+word_freq_file = 'word_freq.txt'
+with open(word_freq_file, encoding="utf-8") as f:
+    for line in f:
+        word, freq = line.strip().split()
+        word_freq[word] = float(freq)
 
-    fqdst = FreqDist(cleaned_words)
+class Word:
+    def __init__(self, word, tf=None, idf=None):
+        self.word = word
+        self.tf = tf
+        self.idf = idf 
+        self.tfidf = tf * idf if tf and idf else None
 
-    return fqdst
+    def __str__(self):
+        out = 'Word: {}, tf: {}, idf: {}, tfidf: {}'.format(self.word, self.tf, self.idf, self.tfidf)
+        return out
 
-KEEP_ENTS = ['DATE', 'PERSON', 'GPE', 'ORG', 'NORP', 'WORK_OF_ART', 'PRODUCT']
-def tag_entities(text):
-    ents = []
-    doc = nlp(text)
-    for token in doc.ents:
-        if token.label_ in KEEP_ENTS:
-            # print(token.text, token.label_)
-            ents.append(token.text)
-    return ents
+class TFIDF:
+
+    def __init__(self, text):
+        self.text = text
+        self.num_words = None
+        self.words = {} 
+
+    def most_common_words(self):
+        num_words = 0
+        ent_list = []
+        doc = nlp(text)
+
+        with doc.retokenize() as retokenizer:
+            for ent in doc.ents:
+                if ent.label_ in KEEP_ENTS:
+                    ent_list.append(ent.text.lower())
+                    retokenizer.merge(doc[ent.start:ent.end])
+
+        cleaned_words = []
+
+        for w in doc:
+            num_words += 1
+            word = w.text.lower()
+            if word not in stop_words and word not in """,.'"!?;:`~--()\n\n""" and word not in ent_list:
+                cleaned_words.append(word) 
+
+        self.num_words = num_words
+        fqdst = FreqDist(cleaned_words)
+        for word, val in fqdst.most_common():
+            tf = val / num_words
+            if word not in word_freq:
+                continue
+            print(word)
+            print(word_freq[word])
+            idf = math.log(word_freq[word]) + 10
+            wd = Word(word, tf=tf, idf=idf)
+            self.words[word] = wd
+        self.freq_dist = fqdst
+        for key, val in self.words.items():
+            print(val)
+        return fqdst
 
 def entity_tag(freq_dist):
     for word, count in freq_dist:
@@ -59,9 +103,5 @@ if __name__ == '__main__':
         sys.exit()
     filename = sys.argv[1]
     text = read_file(filename)
-    # entity_tag(common)
-    ents = tag_entities(text)
-    most_common_words(text, ents)
-    CDFqdst = most_common_words(text, ents)
-    common = CDFqdst.most_common(100)
-    print(common)
+    tfidf = TFIDF(text)
+    tfidf.most_common_words()
