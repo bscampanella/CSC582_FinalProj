85f10203eda7badd57d91f36b16bd7e3a85cf955
Brandon C <bscampnaella99@gmail.com>
2021-06-03 18:22:49 -0700

    changed classifier script to recgonize transforms on text

---
+++ clauseswitch_transform.py
@@ -0,0 +1,122 @@
+import nltk
+from nltk.tag import pos_tag
+import re
+import spacy
+nlp = spacy.load('en_core_web_sm')
+
+
+def sentSegs(taggedSent, grammar, loops = 1):     # sentenceParts; grammar = "NP: {<DT>?<JJ>*<NN>}"
+    #if loops == "":
+    #    loops = "loop=1"
+    cp = nltk.RegexpParser(grammar, loops)
+    result = cp.parse(taggedSent)
+    return result
+
+grammar = r"""
+    NP:
+        {<.*>+}
+        }<IN|CC>+{
+"""   
+
+grammar = r"""                                                              # Mary saw the cat sit on the mat will yield
+  NP: {<DT|JJ|PRP|NN.*>+}          # Chunk sequences of DT, JJ, NN                (S
+  PP: {<IN><NP><>}              # Chunk prepositions followed by NP              (NP Mary/NN)
+  VP: {<MD>?<RB.*>?<VB.*><RB.*>?<NP|PP|RP|CLAUSE>+} # Chunk verbs and their arguments                saw/VBD
+  CLAUSE: {<NP><VP>+}           # Chunk NP, VP                                   (CLAUSE
+""" 
+
+OWL_dependent_markers = set(["after", "although", "as", "as if", "because", "before", "even if", "even though", "if", "in order to", "since", "though", "unless", "until", "whatever", "when", "whenever", "whether", "while"])
+
+
+def clauseswitch(inputtext):
+    punct = inputtext[-1]
+    inputtext_nopunct = inputtext[:-1]
+    sentenceParts = pos_tag(inputtext_nopunct.split()) 
+
+    out = sentSegs(sentenceParts, grammar, 2)
+    new_str = []
+    clause_found = False
+    clause_at = -1
+    for enum, item in enumerate(out):
+        if type(item) == nltk.Tree:
+            if clause_found:
+                for leaf in item.leaves():
+                    new_str.append(leaf[0])
+            else:
+                pass
+        
+        elif type(item) == tuple:
+            if item[0] in OWL_dependent_markers and not clause_found:
+                if (len(out) > enum + 1) and type(out[enum + 1]) == nltk.Tree and out[enum + 1].label() == "CLAUSE":
+                    clause_found = True
+                    clause_at = enum
+                    new_str.append(item[0])
+            
+            elif clause_found:
+                new_str.append(item[0])
+
+    if clause_found:
+        new_str.append(",")
+        flag = False
+        for i in range(clause_at):
+            if type(out[i]) == nltk.Tree:
+                for leaf in out[i].leaves():
+                    if not flag:
+                        flag = True
+                        new_str.append(leaf[0].lower())
+                    else:
+                        new_str.append(leaf[0])
+
+            elif type(out[i]) == tuple:  
+                new_str.append(out[i][0]) 
+        
+
+        new_str[0] = new_str[0].capitalize() 
+        new_str.append(punct)
+
+        return " ".join(new_str)
+
+
+    else:
+        return inputtext
+    
+
+
+
+
+
+
+
+from urllib import request
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+def GetRawText(url):
+    response = request.urlopen(url)
+    raw = response.read().decode('utf8', "ignore")
+    #remove non book stuff
+    start_index = raw.find("***")
+    end_of_line = raw.find("\n", start_index)
+    return raw[end_of_line : ]
+
+
+
+if __name__ == "__main__":
+    print(clauseswitch("He is happy about his car."))
+
+    rawtxt = GetRawText("https://www.gutenberg.org/files/98/98-0.txt")
+    
+    tokens = sent_tokenize(rawtxt)
+
+    print(len(tokens))
+
+    for i, token in enumerate(tokens):
+        
+        sent_after_transform = clauseswitch(token)
+        if sent_after_transform != token:
+
+            print("\n\n-----------------------------------")
+            print(token)
+            print(sent_after_transform)
+
+        if i % 1000 == 0:
+            print(i)
---
+++ not_adj_transform.py
@@ -0,0 +1,104 @@
+from nltk.corpus import wordnet as wn
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+
+def change_sent_not(testsent):
+    pron = ""
+    fin_text = testsent
+    negative_replace = ""
+    doc = nlp(testsent)
+    for i, token in enumerate(doc):
+        if token.pos_ == "PRON":
+            pron = token.text
+
+        if token.lemma_ == "be":
+            cur_text = ""
+            
+            if (len(doc) > i+1) and  (doc[i + 1].pos_ == "ADJ") :
+                cur_text = doc[i + 1].text
+            elif (len(doc) > i+2) and (doc[i + 1].pos_ == "ADV" and doc[i + 2].pos_ == "ADJ"):
+                cur_text = doc[i + 2].text
+            if cur_text != "":
+                cur_synset = None
+                if len(wn.synsets(cur_text, pos="a")):
+                    cur_synset = wn.synsets(cur_text, pos='a')[0]
+                elif len(wn.synsets(cur_text, pos="s")):
+                    cur_synset = wn.synsets(cur_text, pos='s')[0]
+                elif len( wn.synsets(cur_text)):
+                    cur_synset = wn.synsets(cur_text)[0]
+
+
+                if cur_synset:
+                    if (len(cur_synset.hypernyms()) and len(cur_synset.hypernyms()[0].hyponyms())):
+                        for hypo in cur_synset.hypernyms()[0].hyponyms():
+                            if hypo.lemmas()[0].name() != cur_text:
+                                negative_replace = hypo.lemmas()[0].name()
+                                break
+                    elif len(cur_synset.lemmas()):
+                        if len(cur_synset.lemmas()[0].antonyms()):
+                            negative_replace = cur_synset.lemmas()[0].antonyms()[0].name()
+                        else:
+                            for lm in cur_synset.lemmas():
+                                if lm.name() != cur_text:
+                                    negative_replace = lm.name()
+
+            if negative_replace != "":
+                fin_text = ""
+                for x in range(i + 1):
+                    fin_text += doc[x].text + " "
+                
+                fin_text += "not " + negative_replace 
+                #here use the spacy ner to see if a person is in sentence and if so
+                # them change it to they
+                doc2 = nlp(fin_text)
+                ents_lst = []
+                for ent in doc2.ents:
+                    ents_lst.append(ent.text)
+
+                replace_tok = ""
+                if pron:
+                    replace_tok = pron
+                elif ents_lst:
+                    replace_tok = ents_lst[0]
+                else:
+                    replace_tok = "it"
+
+
+                fin_text += ", " + replace_tok + " " + doc[i].text + " "
+                for x in range(i + 1, len(doc)):
+                    fin_text += doc[x].text + " "
+    return fin_text
+
+
+from urllib import request
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+def GetRawText(url):
+    response = request.urlopen(url)
+    raw = response.read().decode('utf8', "ignore")
+    #remove non book stuff
+    start_index = raw.find("***")
+    end_of_line = raw.find("\n", start_index)
+    return raw[end_of_line : ]
+
+
+
+if __name__ == "__main__":
+    print(change_sent_not("He is happy about his car."))
+
+    rawtxt = GetRawText("https://www.gutenberg.org/files/98/98-0.txt")
+    
+    tokens = sent_tokenize(rawtxt)
+
+    print(len(tokens))
+
+    for i, token in enumerate(tokens):
+        
+        sent_after_transform = change_sent_not(token)
+        if sent_after_transform != token:
+            print(token)
+            print(sent_after_transform)
+
+        if i % 1000 == 0:
+            print(i)
